fc9b81ba223fe97d6ad3c6742949ce0552cc5b4f
vinicius espindola <vinicius.c.e@hotmail.com>
2019-12-08 18:10:26 -0300

    fixing makefile path

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ sudo snap install docker
 
 echo "=========> [CLONNING MASTER AND PATCHING] >>>"
 git clone https://github.com/docker/docker-ce
-cd docker-ce
-git apply -v --3way ../patches/*
+cd docker-ce && git apply -v --3way ../patches/*
+cd ../
 
 echo "=========> [BUILDING <$sys> PACKAGES] >>>"
 cd $(pwd)/$dir
