86479813b0554f6109884d379fbe864fc8ff04e8
vinicius espindola <vinicius.c.e@hotmail.com>
2019-12-08 19:49:34 -0300

    .

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 set -e
 
+sudo docker run hello-world
+
 echo "=========> [INSTALLING DOCKER] >>> "
 git clone https://github.com/Unicamp-OpenPower/docker.git
 sudo snap install docker
-sudo service docker stop
-sudo nohup docker daemon -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock &
 
 echo "=========> [CLONNING MASTER AND PATCHING] >>>"
 git clone https://github.com/docker/docker-ce
