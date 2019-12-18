bd3c4161e8d65c5b36d0b0d0a552f7c6a681651e
vinicius espindola <vinicius.c.e@hotmail.com>
2019-12-17 21:55:07 -0300

    Autoremovin no loger required packages

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ sudo docker run hello-world | grep "Hello from Docker!"
 
 echo "=========> [REMOVING DOCKER] >>> "
 sudo apt list --installed | grep docker
-sudo apt-get purge docker.io
+sudo apt-get purge docker.io && sudo apt autoremove
 exit
 
 echo "=========> [CLONNING MASTER AND PATCHING] >>>"
