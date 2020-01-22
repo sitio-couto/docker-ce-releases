496624205301f7262011df22a633ae1d6de29241
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-22 13:11:47 -0300

    retying

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ home=$(pwd)
 echo "=========> [CHECKING DOCKER] >>> "
 docker run hello-world | grep "Hello from Docker!" 
 
-# echo "=========> [CLONNING MASTER AND PATCHING] >>>"
-# git clone https://github.com/docker/docker-ce
-# cd docker-ce && git apply -v --3way ../patches/*
-# cd ../
+echo "=========> [CLONNING MASTER AND PATCHING] >>>"
+git clone https://github.com/docker/docker-ce
+cd docker-ce && git apply -v --3way ../patches/*
+cd ../
 
 # echo "=========> [BUILDING <$sys> PACKAGES] >>>"
 # cd ./$dir
