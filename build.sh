c70865e7983c54ac8f15d247cf3e1d3e8d823500
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-22 18:12:24 -0300

    testing clean install

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@ home=$(pwd)
 
 echo "=========> [UPGRADING DOCKER] >>>"
 sudo lsb_release -a
+docker version
 # wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 # wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 # sudo apt install ./*.deb
+# docker version
 
 echo "=========> [CHECKING DOCKER] >>> "
 docker run hello-world | grep "Hello from Docker!" 
@@ -15,9 +17,9 @@ git clone https://github.com/docker/docker-ce
 cd docker-ce && git apply -v --3way ../patches/*
 cd ../
 
-echo "=========> [BUILDING <$sys> PACKAGES] >>>"
-cd ./$dir
-VERSION=0.0.0 make $sys 
+echo "=========> [BUILDING <ubuntu-bionic> PACKAGES] >>>"
+cd ./docker-ce/components/packaging/deb
+VERSION=0.0.0 make ubuntu-bionic 
 cd ../
 
 # echo "=========> [CHECKING <$sys> PACKAGES] >>>"
