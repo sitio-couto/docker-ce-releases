cbec38a5ce69875eefad8b8ea29fc9eedc4d7421
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-22 13:10:18 -0300

    testing docker only

--- build.sh
+++ build.sh
@@ -7,20 +7,19 @@ home=$(pwd)
 # sudo apt install ./*.deb
 
 echo "=========> [CHECKING DOCKER] >>> "
-sudo docker login
 docker run hello-world | grep "Hello from Docker!" 
 
-echo "=========> [CLONNING MASTER AND PATCHING] >>>"
-git clone https://github.com/docker/docker-ce
-cd docker-ce && git apply -v --3way ../patches/*
-cd ../
+# echo "=========> [CLONNING MASTER AND PATCHING] >>>"
+# git clone https://github.com/docker/docker-ce
+# cd docker-ce && git apply -v --3way ../patches/*
+# cd ../
 
-echo "=========> [BUILDING <$sys> PACKAGES] >>>"
-cd ./$dir
-echo "'make $sys' at '$pwd'"
-echo "-----------------------------------------"
-VERSION=0.0.0 make $sys 
-cd ../
+# echo "=========> [BUILDING <$sys> PACKAGES] >>>"
+# cd ./$dir
+# echo "'make $sys' at '$pwd'"
+# echo "-----------------------------------------"
+# VERSION=0.0.0 make $sys 
+# cd ../
 
-echo "=========> [CHECKING <$sys> PACKAGES] >>>"
-ls $home/$bin_dir
+# echo "=========> [CHECKING <$sys> PACKAGES] >>>"
+# ls $home/$bin_dir
