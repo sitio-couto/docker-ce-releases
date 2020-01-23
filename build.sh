987fff7b7102198e3575d5c4fc4df8d5104cad9c
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-23 06:33:47 -0300

    testing

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sudo apt update && sudo apt upgrade
 echo "=========> [UPGRADING DOCKER] >>>"
 wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
-sudo apt *.deb
+sudo apt install *.deb
 
 echo "=========> [CHECKING ENVIRONMENT] >>> "
 print "\n\nBuild Distro:\n"
