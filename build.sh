1cca4154e0aa808cd73e7b7ff46470021413c229
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-23 06:25:20 -0300

    environment check

--- build.sh
+++ build.sh
@@ -2,14 +2,16 @@ set -e
 home=$(pwd)
 
 echo "=========> [UPGRADING DOCKER] >>>"
-sudo lsb_release -a
-docker version
-# wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
-# wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
-# sudo apt install ./*.deb
-# docker version
+wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
+wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
+sudo apt install ./*.deb
 
-echo "=========> [CHECKING DOCKER] >>> "
+echo "=========> [CHECKING ENVIRONMENT] >>> "
+print "\n\nBuild Distro:\n"
+lsb_release -a
+print "\n\nDocker Version:\n"
+docker version
+print "\n\nDocker Test:\n"
 docker run hello-world | grep "Hello from Docker!" 
 
 echo "=========> [CLONNING MASTER AND PATCHING] >>>"
