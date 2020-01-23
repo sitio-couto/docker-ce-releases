430d7fe691a60d00e158f08b2f716a9c671e16a5
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-23 09:20:50 -0300

    Testing dependencies installation

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 set -e
 home=$(pwd)
 
+echo "=========> [DOCKER DEPENDENCIES] >>>"
+sudo apt install -f 
+ > containerd
+ > iptables
+ > libseccomp2
+ > libc6
+ > libdevmapper1.02.1
+ > libsystemd0
+
+
 # echo "=========> [UPGRADING DOCKER] >>>"
 # printf "\n\Upgrading Docker...\n"
 # wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
