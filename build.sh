01765bdbd3bcb3103bb5efe697188e0e773345d7
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-24 14:28:07 -0300

    testing with no patchs

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
 git clone https://github.com/docker/docker-ce
 cd $home/docker-ce
 git checkout v19.03.5
-git apply -v --3way ../patches/*
+# git apply -v --3way ../patches/*
 
 printf "\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
 cd $home/$dir
