43c9d11073c6e9e9f24c459c79b020391377816c
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-24 14:18:21 -0300

    testing xenial again

--- build.sh
+++ build.sh
@@ -10,19 +10,19 @@ echo "=========> [CHECKING ENVIRONMENT] >>> "
 printf "\nBuild Distro:\n"
 lsb_release -a
 printf "\nDocker Version:\n"
-docker version
+sudo docker version
 printf "\nDocker Test:\n"
 sudo service docker* status
 sudo docker run hello-world | grep "Hello from Docker!" 
 
-printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
-git clone https://github.com/docker/docker-ce
-cd $home/docker-ce
-git apply -v --3way ../patches/*
+# printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
+# git clone https://github.com/docker/docker-ce
+# cd $home/docker-ce
+# git apply -v --3way ../patches/*
 
-printf "\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
-cd $home/$dir
-sudo VERSION=0.0.0 make $sys
+# printf "\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
+# cd $home/$dir
+# sudo VERSION=0.0.0 make $sys
 
 # echo "=========> [CHECKING <$sys> PACKAGES] >>>"
 # ls $home/$bin_dir
