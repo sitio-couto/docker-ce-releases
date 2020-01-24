505dde84506f641dc9cf4c61f56ff28d47f9fbdc
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-01-24 14:23:15 -0300

    testing full xenial

--- build.sh
+++ build.sh
@@ -15,14 +15,14 @@ printf "\nDocker Test:\n"
 sudo service docker* status
 sudo docker run hello-world | grep "Hello from Docker!" 
 
-# printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
-# git clone https://github.com/docker/docker-ce
-# cd $home/docker-ce
-# git apply -v --3way ../patches/*
+printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
+git clone https://github.com/docker/docker-ce
+cd $home/docker-ce
+git apply -v --3way ../patches/*
 
-# printf "\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
-# cd $home/$dir
-# sudo VERSION=0.0.0 make $sys
+printf "\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
+cd $home/$dir
+sudo VERSION=0.0.0 make $sys
 
-# echo "=========> [CHECKING <$sys> PACKAGES] >>>"
-# ls $home/$bin_dir
+echo "=========> [CHECKING <$sys> PACKAGES] >>>"
+ls $home/$bin_dir
