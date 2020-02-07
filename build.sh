57fc532bbce24aa97f8df4a2f408c3c3f9f08b92
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-07 16:12:07 -0300

    removing sudo

--- build.sh
+++ build.sh
@@ -4,26 +4,27 @@ version="19.03.4"
 
 
 echo "=========> [UPGRADING DOCKER] >>>"
-sudo apt update && sudo apt upgrade
+apt update && apt upgrade
+exit
 
-echo "=========> [CHECKING ENVIRONMENT] >>> "
-printf "\nBuild Distro:\n"
-lsb_release -a
-printf "\nDocker Version:\n"
-sudo docker version
-printf "\nDocker Test:\n"
-sudo service docker* status
-sudo docker run hello-world | grep "Hello from Docker!" 
+# echo "=========> [CHECKING ENVIRONMENT] >>> "
+# printf "\nBuild Distro:\n"
+# lsb_release -a
+# printf "\nDocker Version:\n"
+# sudo docker version
+# printf "\nDocker Test:\n"
+# sudo service docker* status
+# sudo docker run hello-world | grep "Hello from Docker!" 
 
-printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
-git clone https://github.com/docker/docker-ce
-cd $home/docker-ce
-git checkout v$version
+# printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
+# git clone https://github.com/docker/docker-ce
+# cd $home/docker-ce
+# git checkout v$version
 # git apply -v --3way ../patches/*
 
-printf "\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
-cd $home/$dir
-sudo VERSION=0.0.0 make $sys
+# printf "\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
+# cd $home/$dir
+# sudo VERSION=0.0.0 make $sys
 
-echo "=========> [CHECKING <$sys> PACKAGES] >>>"
-ls $home/$bin_dir
+# echo "=========> [CHECKING <$sys> PACKAGES] >>>"
+# ls $home/$bin_dir
