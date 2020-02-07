5940cd24990ac6d2ab333a939de92d22b0bb917c
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-07 16:52:54 -0300

    Testing docker installation

--- build.sh
+++ build.sh
@@ -5,18 +5,20 @@ home=$(pwd)
 version="19.03.4"
 
 
-echo "=========> [UPGRADING DOCKER] >>>"
-sudo apt update && sudo apt upgrade
-exit
+echo "=========> [INSTALLING DOCKER] >>>"
+sudo apt update
+wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
+wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
+sudo apt install ./docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb ./docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 
-# echo "=========> [CHECKING ENVIRONMENT] >>> "
-# printf "\nBuild Distro:\n"
-# lsb_release -a
-# printf "\nDocker Version:\n"
-# sudo docker version
-# printf "\nDocker Test:\n"
-# sudo service docker* status
-# sudo docker run hello-world | grep "Hello from Docker!" 
+echo "=========> [CHECKING ENVIRONMENT] >>> "
+printf "\nBuild Distro:\n"
+lsb_release -a
+printf "\nDocker Version:\n"
+sudo docker version
+printf "\nDocker Test:\n"
+sudo service docker* status
+sudo docker run hello-world | grep "Hello from Docker!" 
 
 # printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
 # git clone https://github.com/docker/docker-ce
