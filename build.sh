8e44ffca3decdead62d031c893403063b60c2969
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-07 17:04:09 -0300

    testing build

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 
 set -e
 home=$(pwd)
-version="19.03.4"
 
-
-echo "=========> [INSTALLING DOCKER] >>>"
+printf "\n\n=========> [INSTALLING DOCKER] >>>\n"
 sudo apt update
 wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
@@ -13,7 +11,7 @@ sudo apt install -y ./docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 sudo apt install -y ./docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 rm -f *.deb
 
-echo "=========> [CHECKING ENVIRONMENT] >>> "
+printf "\n\n=========> [CHECKING ENVIRONMENT] >>>\n"
 printf "\nBuild Distro:\n"
 lsb_release -a
 printf "\nDocker Version:\n"
@@ -22,15 +20,14 @@ printf "\nDocker Test:\n"
 sudo service docker* status
 sudo docker run hello-world | grep "Hello from Docker!" 
 
-# printf "\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
-# git clone https://github.com/docker/docker-ce
-# cd $home/docker-ce
-# git checkout v$version
-# git apply -v --3way ../patches/*
+printf "\n\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
+git clone https://github.com/docker/docker-ce
+cd $home/docker-ce
+git apply -v --3way ../patches/*
 
-# printf "\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
-# cd $home/$dir
-# sudo VERSION=0.0.0 make $sys
+printf "\n\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
+cd $home/$dir
+sudo VERSION=0.0.0 make $sys
 
-# echo "=========> [CHECKING <$sys> PACKAGES] >>>"
-# ls $home/$bin_dir
+printf "\n\n=========> [CHECKING <$sys> PACKAGES] >>>\n"
+ls $home/$bin_dir
