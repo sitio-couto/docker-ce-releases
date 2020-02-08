e47de8ca99eeece86749f90d579623021e473120
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-08 06:33:24 -0300

    Removing e flag

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-set -e
 home=$(pwd)
 bin_dir=docker-ce/components/packaging/deb/debbuild/debian-stretch/
 dir=docker-ce/components/packaging/deb
 sys=ubuntu-bionic
 
 printf "\n\n=========> [INSTALLING DOCKER] >>>\n"
-sudo apt update
+sudo apt update && sudo apt upgrade
 wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
 sudo apt install -y ./docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb 
@@ -20,7 +19,6 @@ lsb_release -a
 printf "\nDocker Version:\n"
 sudo docker version
 printf "\nDocker Test:\n"
-sudo service docker* status
 sudo docker run hello-world | grep "Hello from Docker!" 
 
 printf "\n\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
