42832e2f6792880377e3d31d0343f53721b155a3
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-09 19:35:19 -0300

    Adding error flag

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+set -e
 home=$(pwd)
 
 printf "\n\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
 git clone https://github.com/docker/docker-ce
 cd $home/docker-ce
-git checkout v19.03.5
-# git apply -v --3way ../patches/*
+git apply -v --3way ../patches/*
 
 printf "\n\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
 cd $home/$dir
-sudo VERSION=19.03.5 make $sys
+sudo make $sys
 
 printf "\n\n=========> [CHECKING <$sys> PACKAGES] >>>\n"
 ls $home/$bin_dir
