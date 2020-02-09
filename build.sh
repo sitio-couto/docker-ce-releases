d577cf24cdb45ff5e1939722bef8969baaf265b4
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-09 06:41:18 -0300

    Removing unnecessary stuff

--- build.sh
+++ build.sh
@@ -5,23 +5,6 @@ bin_dir=docker-ce/components/packaging/deb/debbuild/ubuntu-bionic/
 dir=docker-ce/components/packaging/deb
 sys=ubuntu-bionic
 
-printf "\n\n=========> [INSTALLING DOCKER] >>>\n"
-sudo apt update && sudo apt upgrade
-wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
-wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.5/ubuntu-bionic/docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
-sudo apt install -y ./docker-ce-cli_19.03.5~3-0~ubuntu-bionic_ppc64el.deb 
-sudo apt install -y ./docker-ce_19.03.5~3-0~ubuntu-bionic_ppc64el.deb
-rm -f *.deb
-
-printf "\n\n=========> [CHECKING ENVIRONMENT] >>>\n"
-printf "\nBuild Distro:\n"
-lsb_release -a
-printf "\nDocker Version:\n"
-sudo docker version
-printf "\nDocker Test:\n"
-sudo systemctl is-active docker
-sudo docker run hello-world | grep "Hello from Docker!" 
-
 printf "\n\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
 git clone https://github.com/docker/docker-ce
 cd $home/docker-ce
