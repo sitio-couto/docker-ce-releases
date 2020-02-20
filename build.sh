3026359cff113923e8619b2c5552e292be047364
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-20 08:19:21 -0300

    Branch for building old versions

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ home=$(pwd)
 printf "\n\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
 git clone https://github.com/docker/docker-ce
 cd $home/docker-ce
+git checkout v$ver
 git am --3way ../patches/*
 
 printf "\n\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
 cd $home/$dir
-sudo VERSION=0.0.0-dev make $sys
+sudo VERSION=$ver make $sys
 
 printf "\n\n=========> [CHECKING <$sys> PACKAGES] >>>\n"
 ls $home/$bin_dir
