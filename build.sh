aeee13420c5a428812eab0b322e5078b53f35930
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-20 07:35:32 -0300

    Changed patching command

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ home=$(pwd)
 printf "\n\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
 git clone https://github.com/docker/docker-ce
 cd $home/docker-ce
-git apply -v --3way ../patches/*
+git am --3way ../patches/*
 
 printf "\n\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
 cd $home/$dir
