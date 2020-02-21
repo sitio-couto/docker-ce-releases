19a2750cfe146d2a8bbd8c4bf933d63492ca860a
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-02-20 22:59:41 -0300

    removing untracked files

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ printf "\n\n=========> [CLONNING MASTER AND PATCHING] >>>\n"
 git clone https://github.com/docker/docker-ce
 cd $home/docker-ce
 git checkout v$ver
+git clean -f
 git am --3way ../patches/*
 
 printf "\n\n=========> [BUILDING <$sys> PACKAGES] >>>\n"
